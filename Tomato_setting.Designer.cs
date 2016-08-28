21660f5bd5f6b2bacf6ab0f7c871bb6237a87713
Yu Wenhui <alucardlockon3@gmail.com>
2016-08-28 22:45:20 +0800

    commit version 0.5

--- Tomato_setting.Designer.cs
+++ Tomato_setting.Designer.cs
@@ -28,19 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tomato_tm = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.break_tm = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tomato_cycle = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.long_break_tm = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "工作时间(秒)";
+            // 
+            // tomato_tm
+            // 
+            this.tomato_tm.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tomato_tm.Location = new System.Drawing.Point(134, 35);
+            this.tomato_tm.Name = "tomato_tm";
+            this.tomato_tm.Size = new System.Drawing.Size(100, 21);
+            this.tomato_tm.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "休息时间(秒)";
+            // 
+            // break_tm
+            // 
+            this.break_tm.Location = new System.Drawing.Point(134, 75);
+            this.break_tm.Name = "break_tm";
+            this.break_tm.Size = new System.Drawing.Size(100, 21);
+            this.break_tm.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(66, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "循环次数";
+            // 
+            // tomato_cycle
+            // 
+            this.tomato_cycle.Location = new System.Drawing.Point(134, 155);
+            this.tomato_cycle.Name = "tomato_cycle";
+            this.tomato_cycle.Size = new System.Drawing.Size(100, 21);
+            this.tomato_cycle.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(159, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "取消";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "长休息时间(秒)";
+            // 
+            // long_break_tm
+            // 
+            this.long_break_tm.Location = new System.Drawing.Point(134, 116);
+            this.long_break_tm.Name = "long_break_tm";
+            this.long_break_tm.Size = new System.Drawing.Size(100, 21);
+            this.long_break_tm.TabIndex = 9;
             // 
             // Tomato_setting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.long_break_tm);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tomato_cycle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.break_tm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tomato_tm);
+            this.Controls.Add(this.label1);
             this.Name = "Tomato_setting";
             this.Text = "设置";
+            this.Load += new System.EventHandler(this.Tomato_setting_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tomato_tm;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox break_tm;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tomato_cycle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox long_break_tm;
     }
 }
--- tomato_work.cs
+++ tomato_work.cs
@@ -14,15 +14,21 @@ namespace CodeNote
 {
     public partial class Tomato_work : Form
     {
+        private static Tomato_setting tomato_setting;
+
         private string time_state = "init";
         private DateTime time = new DateTime();
         private DateTime timenow = new DateTime();
         private TimeSpan tmspan=new TimeSpan();
-        private long tomato_now_cylce = 0;
-
+        private int tomato_now_cylce = 1;
+        private int tomato_count = 0;
+        //cfg参数
         private int cfg_tomato_tm = 1500;
         private int cfg_break_tm = 300;
+        private int cfg_long_break_tm = 900;
         private int cfg_tomato_cylce = 4;
+        //user参数
+
         public Tomato_work()
         {
             InitializeComponent();
@@ -31,10 +37,10 @@ namespace CodeNote
         private void Tomato_work_Load(object sender, EventArgs e)
         {
             time_label.Text = "00:00";
+            
             string lists = GetXmlConfigList("config/tomato_list.xml", "/list/task");
-            cfg_tomato_tm = Convert.ToInt32(GetXmlConfig("config/tomato_cfg.xml", "/config/tomato_tm"));
-            cfg_break_tm = Convert.ToInt32(GetXmlConfig("config/tomato_cfg.xml", "/config/break_tm"));
-            cfg_tomato_cylce = Convert.ToInt32(GetXmlConfig("config/tomato_cfg.xml", "/config/tomato_cycle"));
+
+            Init();
         }
 
         
@@ -49,7 +55,26 @@ namespace CodeNote
                     time_label.Text = tmspan.Minutes + ":" + tmspan.Seconds;
                 }else {
                     timenow = DateTime.Now;
-                    time = DateTime.Now.AddMinutes(5);
+                    if (tomato_now_cylce < cfg_tomato_cylce)
+                    {
+                        time = DateTime.Now.AddSeconds(cfg_break_tm);
+                        now_state_lb.Text = "休息时间";
+                    }
+                    else
+                    {
+                        time = DateTime.Now.AddSeconds(cfg_long_break_tm);
+                        now_state_lb.Text = "长休息时间";
+                    }
+                    tomato_count++;
+                    if (GetXmlConfig("config/tomato_user.xml", "/user/tomato_today_date") != DateTime.Now.ToString("yyyy-MM-dd"))
+                    {
+                        SetXmlConfig("config/tomato_user.xml", "/user/tomato_today", "0");
+                        SetXmlConfig("config/tomato_user.xml", "/user/tomato_today_date", DateTime.Now.ToString("yyyy-MM-dd"));
+                    }
+                    SetXmlConfig("config/tomato_user.xml", "/user/tomato_today", Convert.ToString(Convert.ToInt32(GetXmlConfig("config/tomato_user.xml", "/user/tomato_today")) + 1));
+                    SetXmlConfig("config/tomato_user.xml", "/user/tomato_count", Convert.ToString(Convert.ToInt32(GetXmlConfig("config/tomato_user.xml", "/user/tomato_count")) + 1));
+                    total_tomato_cnt_lb.Text = "番茄数:" + tomato_count;
+                    today_tomato_cnt_lb.Text = "今日:" + GetXmlConfig("config/tomato_user.xml", "/user/tomato_today");
                     time_state = "running_break";
                 }
             }
@@ -62,8 +87,18 @@ namespace CodeNote
                 }
                 else
                 {
-                    time_state="stop";
-                    time_label.Text = "00:00";
+                    if(tomato_now_cylce<cfg_tomato_cylce){
+                        tomato_now_cylce++;
+                    }else{
+                        tomato_now_cylce = 1 ;
+                        SetXmlConfig("config/tomato_user.xml", "/user/total_cycle", Convert.ToString(Convert.ToInt32(GetXmlConfig("config/tomato_user.xml", "/user/total_cycle")) + 1));
+                        total_cycle_lb.Text = "总循环:" + GetXmlConfig("config/tomato_user.xml", "/user/total_cycle");
+                    }
+                    cycle_count_lb.Text = "循环:" + tomato_now_cylce + "/" + cfg_tomato_cylce;
+                    timenow = DateTime.Now;
+                    time = DateTime.Now.AddSeconds(cfg_tomato_tm);
+                    time_state = "running_tm";
+                    now_state_lb.Text = "工作时间";
                 }
             }
         }
@@ -85,6 +120,7 @@ namespace CodeNote
                 time = DateTime.Now.AddSeconds(cfg_tomato_tm);
             }
             time_state = "running_tm";
+            now_state_lb.Text = "工作时间";
             timer1.Enabled = true;
         }
         /*
@@ -94,6 +130,7 @@ namespace CodeNote
         {
             timer1.Enabled = false;
             time_state = "pause";
+            now_state_lb.Text = "暂停";
         }
         /*
          * 重新开始 
@@ -103,6 +140,21 @@ namespace CodeNote
             timenow = DateTime.Now;
             time = DateTime.Now.AddSeconds(cfg_tomato_tm);
             time_state = "running_tm";
+            now_state_lb.Text = "工作时间";
+            timer1.Enabled = true;
+        }
+
+        /*
+         * 重新开始循环 
+         */
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            timenow = DateTime.Now;
+            time = DateTime.Now.AddSeconds(cfg_tomato_tm);
+            time_state = "running_tm";
+            now_state_lb.Text = "工作时间";
+            tomato_now_cylce = 1;
+            cycle_count_lb.Text = "循环:" + tomato_now_cylce + "/" + cfg_tomato_cylce;
             timer1.Enabled = true;
         }
         /*
@@ -110,23 +162,49 @@ namespace CodeNote
          */
         private void toolStripLabel4_Click(object sender, EventArgs e)
         {
-
+            if (tomato_setting == null || tomato_setting.IsDisposed)
+            {
+                tomato_setting = new Tomato_setting(this);
+                tomato_setting.Show();
+            }
         }
 
-
+        public void Init()
+        {
+            //初始化cfg参数
+            cfg_tomato_tm = Convert.ToInt32(GetXmlConfig("config/tomato_cfg.xml", "/config/tomato_tm"));
+            cfg_break_tm = Convert.ToInt32(GetXmlConfig("config/tomato_cfg.xml", "/config/break_tm"));
+            cfg_long_break_tm = Convert.ToInt32(GetXmlConfig("config/tomato_cfg.xml", "/config/long_break_tm"));
+            cfg_tomato_cylce = Convert.ToInt32(GetXmlConfig("config/tomato_cfg.xml", "/config/tomato_cylce"));
+            //初始化界面
+            cycle_count_lb.Text = "循环:" + tomato_now_cylce + "/" + cfg_tomato_cylce;
+            tomato_count = Convert.ToInt32(GetXmlConfig("config/tomato_user.xml", "/user/tomato_count"));
+            today_tomato_cnt_lb.Text = "今日:" + GetXmlConfig("config/tomato_user.xml", "/user/tomato_today");
+            total_tomato_cnt_lb.Text = "番茄数:" + tomato_count;
+            total_cycle_lb.Text = "总循环:" + GetXmlConfig("config/tomato_user.xml", "/user/total_cycle");
+        }
 
         private string GetXmlConfigList(string filename,string xpath)
         {
             XmlDocument doc = new XmlDocument();
             doc.Load(filename);
-            return doc.SelectNodes(xpath)[0].InnerText;
+            return doc.SelectNodes(xpath)[0].InnerText.Trim();
         }
 
         private string GetXmlConfig(string filename, string xpath)
         {
             XmlDocument doc = new XmlDocument();
             doc.Load(filename);
-            return doc.SelectSingleNode(xpath).InnerText;
+            return doc.SelectSingleNode(xpath).InnerText.Trim();
         }
+
+        private void SetXmlConfig(string filename, string xpath, string value)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filename);
+            doc.SelectSingleNode(xpath).InnerText = value.Trim();
+            doc.Save(filename);
+        }
+
     }
 }
